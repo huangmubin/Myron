37ded430c1f700ad1dbfc62f76ed2f5d89bfadd6
huangmubin <huangmubin@qq.com>
2016-08-19 13:45:42 +0800

    Create iOS Test Product.

---
+++ iOS_Test_Product/iOS_Test_Product/TmpFile.swift
@@ -0,0 +1,13 @@
+//
+//  TmpFile.swift
+//  iOS_Test_Product
+//
+//  Created by 黄穆斌 on 16/8/19.
+//  Copyright © 2016年 Myron. All rights reserved.
+//
+
+import UIKit
+
+class TmpFile: NSObject {
+
+}
---
+++ iOS_Test_Product/iOS_Test_Product/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS_Test_Product
+//
+//  Created by 黄穆斌 on 16/8/19.
+//  Copyright © 2016年 Myron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
