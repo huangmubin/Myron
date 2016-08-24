488019c2bee446c7e4f287f0e480ea844d059f57
huangmubin <huangmubin@qq.com>
2016-08-24 18:00:46 +0800

    update.

--- iOS_Test_Product/iOS_Test_Product/ViewController.swift
+++ iOS_Test_Product/iOS_Test_Product/ViewController.swift
@@ -8,51 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ViewController: UIViewController {
+    
+    var carousel: Carousel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(NSHomeDirectory())
-        //Explorer.shared.clearTmp()
-//        for i in 0 ..< 13 {
-//            let data = NSData(contentsOfFile: "/Users/Myron/Desktop/未命名文件夹/\(i).png")
-//            Explorer.shared.save(data, name: "save_\(i).png", time: 1, folder: "Saves/", replace: false, infos: nil, cache: true)
-//        }
+        carousel = Carousel(frame: view.bounds)
+        carousel.backgroundColor = UIColor.redColor()
+        view.addSubview(carousel)
+        
+        for i in 1 ..< 7 {
+            let model = CarouselModel()
+            model.image = UIImage(named: "\(i)")
+            model.name = "\(i)"
+            carousel.models.append(model)
+        }
+        
+        carousel.reload()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        //Explorer.shared.clearCache()
     }
 
     @IBAction func lefitAction(sender: UIBarButtonItem) {
-        //Explorer.shared.clearTmp()
-        
+        carousel.direction = !carousel.direction
     }
 
     @IBAction func rightAction(sender: UIBarButtonItem) {
-        // print("Cache Size = \(Explorer.shared.cache.size); Cache count = \(Explorer.shared.cache.queue.count);")
-    }
-    
-    
-    
-    // MARK: - TableView
-    
-    @IBOutlet weak var tableView: UITableView!
-    
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 13
-    }
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
-        if let data = Explorer.shared.read("Saves/", name: "save_\(indexPath.row).png") {
-            let image = UIImage(data: data)
-            cell.imageView?.image = image
-        } else {
-            cell.imageView?.image = nil
-        }
-        return cell
+        
     }
     
 }
