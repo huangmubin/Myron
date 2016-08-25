01087f0697b13996af97558160daf3a88808e34e
huangmubin <huangmubin@qq.com>
2016-08-25 18:01:03 +0800

    update.

--- Test Products/iOS_Test_Product/iOS_Test_Product/ViewController.swift
+++ Test Products/iOS_Test_Product/iOS_Test_Product/ViewController.swift
@@ -8,37 +8,72 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class Transion: NSObject, UIViewControllerAnimatedTransitioning {
     
-    var carousel: Carousel!
+    func transitionDuration(transitionContext: UIViewControllerContextTransitioning?) -> NSTimeInterval {
+        return 0.3
+    }
+    
+    func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
+        
+    }
+    
+    func animationEnded(transitionCompleted: Bool) {
+        
+    }
+}
+
+class ViewController: UIViewController, UINavigationControllerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        carousel = Carousel(frame: view.bounds)
-        carousel.backgroundColor = UIColor.redColor()
-        view.addSubview(carousel)
+        self.navigationController?.delegate = self
         
-        for i in 1 ..< 7 {
-            let model = CarouselModel()
-            model.image = UIImage(named: "\(i)")
-            model.name = "\(i)"
-            carousel.models.append(model)
+        sub.frame = CGRect(x: 50, y: 50, width: 50, height: 50)
+        sub.backgroundColor = UIColor.brownColor()
+        view.addSubview(sub)
+    }
+
+    let sub = UIView()
+    
+    func navigationController(navigationController: UINavigationController, animationControllerForOperation operation: UINavigationControllerOperation, fromViewController fromVC: UIViewController, toViewController toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        if operation == UINavigationControllerOperation.Push {
+            return nil
+        } else {
+            return nil
         }
-        
-        carousel.reload()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-
+    
     @IBAction func lefitAction(sender: UIBarButtonItem) {
-        carousel.direction = !carousel.direction
+        
     }
 
     @IBAction func rightAction(sender: UIBarButtonItem) {
-        carousel.remove()
+        
+    }
+    
+    var begin: ((view: UIView) -> Void)? = {
+        $0.frame = CGRect(x: 100, y: 200, width: 100, height: 100)
+    }
+    var end: ((Bool) -> Void)? = {
+        print("end = \($0)")
+    }
+    @IBAction func action(sender: UIButton) {
+        //self.performSegueWithIdentifier("Segue", sender: nil)
+        UIView.animateWithDuration(10, animations: { 
+            self.begin?(view: self.sub)
+            }, completion: end)
+    }
+    
+    @IBAction func slider(sender: UISlider) {
+    }
+}
+
+class ViewController2: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.blueColor()
     }
     
 }
-
